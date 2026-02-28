391998eabfe4c49750e80afb796a3df4cf65cd7a
Arnav pratap singh <arnavpratapsingh2006@gmail.com>
2026-02-28 18:28:12 +0530

    api testing on postman

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -3,7 +3,7 @@ import { registerUser } from "../controllers/user.controller.js";
 
 const router=Router()
 
-router.route("/register").post(registerUser)
-// https/localhost:8000/users/register --> after /users control will be given to /register
+router.route("/register").post(registerUser) // now in postman check post type request and enter the address you'll see whether your api is working or not
+// https://localhost:8000/users/register --> after /users control will be given to /register
 
 export default router
--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -63,7 +63,7 @@ Used when the inner function (func) involves asynchronous operations.
 // Method - 2 (using promises)
 
 // const asyncHandler=(fn)=>{
-//     (req,res,next)=>{
+//     return (req,res,next)=>{
 //         Promise
 //         .resolve(fn(req,res,next))
 //         .catch((error)=>next(error))
